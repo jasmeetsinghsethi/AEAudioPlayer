e807c4707d0ff02b3b0f82f743af8c8d4ffafe72
Daniel Jackson <danielmj@me.com>
2014-11-20 19:45:49 -0800

    Initial Commit

---
+++ AAudioPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AAudioPlayer
+//
+//  Created by Daniel Jackson on 11/20/14.
+//  Copyright (c) 2014 Daniel Jackson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AAudioPlayer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AAudioPlayer
+//
+//  Created by Daniel Jackson on 11/20/14.
+//  Copyright (c) 2014 Daniel Jackson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
