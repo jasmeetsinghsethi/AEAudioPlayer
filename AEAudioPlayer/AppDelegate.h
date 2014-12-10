3b2ef733f3b16c3f3e99e5d68c5b1a95f9c67bd8
Daniel Jackson <danielmj@me.com>
2014-12-10 13:35:41 -0800

    Remaming Project

---
+++ AEAudioPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AEAudioPlayer
+//
+//  Created by Daniel Jackson on 12/10/14.
+//  Copyright (c) 2014 Daniel Jackson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
